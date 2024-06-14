5d0feca759fd11b39ea6b4c75504aefb1fa17d41
Rookie9962 <150514029+Rookie9962@users.noreply.github.com>
2024-06-14 07:36:30 -0400

    Create connections_config.py

---
+++ src/connections_config.py
@@ -0,0 +1,6 @@
+
+# Conexión a la base de datos de akademos
+akademos_data_base = 'postgresql://postgres:1234@localhost:5432/default-ak-celery-2023-10-13-094033'
+
+# Conexión a la base de datos destino (Data Warehouse Almacen de Datos)
+datamart_data_base = 'postgresql://postgres:1234@localhost:5432/almacen_fortes_tesis'
